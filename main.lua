19888a7bf7963dd74483ce7132ead6c958bd59c8
Mauritz Sverredal <msverredal@example.org>
2023-03-26 05:35:05 +0200

    Create functions as local instead of global

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@ notice and this notice are preserved.  This file is offered as-is,
 without any warranty.
 ]]--
 
-function refreshBag()
+local function refreshBag()
     bag = {}
     for i = 1, 7 do
         bag[i] = i
@@ -18,12 +18,34 @@ function refreshBag()
     end
     return bag
 end
-function lockPiece(piece)
+local function lockPiece(piece)
     for _,s in pairs(piece.squares) do
         field[s.x][s.y] = s
     end
 end
-function getPiece()
+local function makePiece(id, isShadow)
+    local piece = {}
+    piece.squares = {}
+    for i, s in ipairs(pieces[id]) do
+        piece.squares[i] = {}
+        piece.squares[i].y = s.y + field.height - 2
+        piece.squares[i].x = s.x + math.ceil(field.width / 2) - 2
+        piece.squares[i].color = pieces[id].color
+        piece.squares[i].shape = s.shape
+        piece.squares[i].rotation = s.rotation
+        if isShadow then
+            piece.squares[i].type = "shadow"
+        else
+            piece.squares[i].type = "normal"
+        end
+    end
+    piece.rotatePointY = field.height - 2 + pieces[id].rotateY
+    piece.rotatePointX = math.ceil(field.width / 2) - 2 + pieces[id].rotateX
+    piece.color = pieces[id].color
+    piece.id = id
+    return(piece)
+end
+local function getPiece()
     currentPiece = makePiece(nextPiece, false)
     nextPiece = bag[1]
     if #bag == 1 then
@@ -42,7 +64,40 @@ function getPiece()
         end
     end
 end
-function newGame() -- (re)sets the game state
+local function movePiece(piece, x, y)
+    for _,s in pairs(piece.squares) do
+        if s.y - y < 1 or s.x + x < 1 or s.x + x > field.width or field[s.x + x][s.y - y].type == "normal" or field[s.x][s.y].type == "normal" then
+            return true
+        end
+    end
+    for _,s in pairs(piece.squares) do
+        s.y = s.y - y
+        s.x = s.x + x
+    end
+    piece.rotatePointY = currentPiece.rotatePointY - y
+    piece.rotatePointX = currentPiece.rotatePointX + x
+    return false
+end
+local function updateShadow()
+    shadow = makePiece(currentPiece.id, true)
+    for i = 1, field.width do
+        for l = 1, field.height do
+            if field[i][l].type == "shadow" then
+                field[i][l] = {
+                    color = 0,
+                    type = "empty"
+                }
+            end
+        end
+    end
+    for i,s in pairs(shadow.squares) do
+        s.x = currentPiece.squares[i].x
+        s.y = currentPiece.squares[i].y
+    end
+    while not (movePiece(shadow, 0, 1)) do end
+    lockPiece(shadow)
+end
+local function newGame() -- (re)sets the game state
     score = 0
     showScore = true -- Show score on screen
 
@@ -83,21 +138,7 @@ function newGame() -- (re)sets the game state
         love.audio.play(TETRIS) -- Restart music
     end
 end
-function movePiece(piece, x, y)
-    for _,s in pairs(piece.squares) do
-        if s.y - y < 1 or s.x + x < 1 or s.x + x > field.width or field[s.x + x][s.y - y].type == "normal" or field[s.x][s.y].type == "normal" then
-            return true
-        end
-    end
-    for _,s in pairs(piece.squares) do
-        s.y = s.y - y
-        s.x = s.x + x
-    end
-    piece.rotatePointY = currentPiece.rotatePointY - y
-    piece.rotatePointX = currentPiece.rotatePointX + x
-    return false
-end
-function setSquareSize()
+local function setSquareSize()
     local width = field.width
     local height = field.height
     if showScore then
@@ -108,7 +149,7 @@ function setSquareSize()
         love.graphics.getWidth() / width
     ))
 end
-function squareWindow()
+local function squareWindow()
     setSquareSize()
     local width = field.width
     local height = field.height
@@ -119,29 +160,7 @@ function squareWindow()
     text1x = love.graphics.newFont(squareSize)
     text2x = love.graphics.newFont(squareSize * 2)
 end
-function makePiece(id, isShadow)
-    local piece = {}
-    piece.squares = {}
-    for i, s in ipairs(pieces[id]) do
-        piece.squares[i] = {}
-        piece.squares[i].y = s.y + field.height - 2
-        piece.squares[i].x = s.x + math.ceil(field.width / 2) - 2
-        piece.squares[i].color = pieces[id].color
-        piece.squares[i].shape = s.shape
-        piece.squares[i].rotation = s.rotation
-        if isShadow then
-            piece.squares[i].type = "shadow"
-        else
-            piece.squares[i].type = "normal"
-        end
-    end
-    piece.rotatePointY = field.height - 2 + pieces[id].rotateY
-    piece.rotatePointX = math.ceil(field.width / 2) - 2 + pieces[id].rotateX
-    piece.color = pieces[id].color
-    piece.id = id
-    return(piece)
-end
-function copyPiece(piece)
+local function copyPiece(piece)
     -- Creates a copy of a piece, since Lua tables are always passed by reference
     local newPiece = {}
     newPiece.squares = {}
@@ -208,7 +227,7 @@ function love.resize()
     text1x = love.graphics.newFont(squareSize)
     text2x = love.graphics.newFont(squareSize * 2)
 end
-function makeRotatedPiece(piece, direction)
+local function makeRotatedPiece(piece, direction)
     local rotatedPiece = copyPiece(piece)
     for _, s in pairs(rotatedPiece.squares) do
         local storedX = s.x
@@ -219,7 +238,7 @@ function makeRotatedPiece(piece, direction)
     end
     return rotatedPiece
 end
-function rotateCurrentPiece(direction)
+local function rotateCurrentPiece(direction)
     local rotatedPiece = makeRotatedPiece(currentPiece, direction)
     canSpin = true
     for _, s in pairs(rotatedPiece.squares) do
@@ -235,6 +254,29 @@ function rotateCurrentPiece(direction)
     if canSpin then
         currentPiece = rotatedPiece
         updateShadow()
+    end
+end
+local function checkLines()
+    linesCleared = 0
+    for l = 1, field.height do
+        clearLine = true
+        for i = 1, field.width do
+            if field[i][field.height + 1 - l].color < 1 then
+                clearLine = false
+                break
+            end
+        end
+        if clearLine == true then
+            linesCleared = linesCleared + 1
+            for j = field.height + 1 - l, field.height + 1 do
+                for k = 1, field.width do
+                    field[k][j] = field[k][j + 1]
+                end
+            end
+        end
+    end
+    if linesCleared > 0 then
+        score = score + 3 ^ (linesCleared - 1) * 100
     end
 end
 function love.keypressed(key)
@@ -276,7 +318,7 @@ function love.keypressed(key)
             hasHeld = false
         elseif key == "z" then
             rotateCurrentPiece(1)
-        elseif key == "x" then 
+        elseif key == "x" then
             rotateCurrentPiece(-1)
         end
     end
@@ -319,48 +361,6 @@ function love.update(dt)
         end
         t = t + dt
     end
-end
-function checkLines()
-    linesCleared = 0
-    for l = 1, field.height do
-        clearLine = true
-        for i = 1, field.width do
-            if field[i][field.height + 1 - l].color < 1 then
-                clearLine = false
-                break
-            end
-        end
-        if clearLine == true then
-            linesCleared = linesCleared + 1
-            for j = field.height + 1 - l, field.height + 1 do
-                for k = 1, field.width do
-                    field[k][j] = field[k][j + 1]
-                end
-            end
-        end
-    end
-    if linesCleared > 0 then
-        score = score + 3 ^ (linesCleared - 1) * 100
-    end
-end
-function updateShadow()
-    shadow = makePiece(currentPiece.id, true)
-    for i = 1, field.width do
-        for l = 1, field.height do
-            if field[i][l].type == "shadow" then
-                field[i][l] = {
-                    color = 0,
-                    type = "empty"
-                }
-            end
-        end
-    end
-    for i,s in pairs(shadow.squares) do
-        s.x = currentPiece.squares[i].x
-        s.y = currentPiece.squares[i].y
-    end
-    while not (movePiece(shadow, 0, 1)) do end
-    lockPiece(shadow)
 end
 function inCurrentPiece(x, y)
     for _, s in pairs(currentPiece.squares) do
@@ -421,7 +421,7 @@ function drawField(x0, y0)
     end
     love.graphics.pop()
 end
-function drawCurrentPiece(fieldOffsetX, fieldOffsetY)
+local function drawCurrentPiece(fieldOffsetX, fieldOffsetY)
     love.graphics.push()
     love.graphics.translate(fieldOffsetX, fieldOffsetY)
     love.graphics.scale(1, -1)
